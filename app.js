336d2c8861ad87b4eb395c86da4b2c68cc6abef4
ali.abukahil <ali.abukahil@gmail.com>
2021-08-19 19:53:21 +0200

    Refactoring Our Routes

--- app.js
+++ app.js
@@ -8,29 +8,12 @@ const app = express();
 // middleware is basically just a function that can modify the incoming request data
 // it is called middleware because it stands between, so in the middle of the request and the response
 app.use(express.json());
-//////////////////////////////////////////
-
-// // Defining Route, all we do is app then the http method (get) we want to respond to
-// // get is http method
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side!', app: 'Natours' });
-// });
-
-// // post is also http method
-// // this is how we send different responses
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// the route handler
-// get method to get a new tour
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     // optional when sending multiple responses, to include a field called results to show how many results are there, only makes sense whenever we're sending an array
@@ -39,9 +22,9 @@ app.get('/api/v1/tours', (req, res) => {
     // in ES6 we don't have to specify the KEY and the VALUE if they are both the same name like this for Example data: { tours: tours },
     data: { tours },
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find((element) => element.id === id);
@@ -60,11 +43,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-// post method to create a new tour
-// the req is what hold all the data
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   // console.log(req.body);
 
   const newId = tours[tours.length - 1].id + 1;
@@ -84,8 +65,62 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
 
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here ...',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  // status (204) means no content
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+// the route handler
+// get http method to get a new tour
+// app.get('/api/v1/tours', getAllTours);
+//app.get('/api/v1/tours/:id', getTour);
+// post http method to create a new tour
+// the req is what hold all the data
+//app.post('/api/v1/tours', createTour);
+// to update data we have two http methods
+// we have (Put) and we (Patch)
+// And with Put, we expect that our application receives the entire updated object
+// And with Patch, we only expect the properties that should be updated on the object
+//app.patch('/api/v1/tours/:id', updateTour);
+// Delete http method
+//app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// the server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
