8bec2f780e95c56893581b3c608f9c8570957e91
ali.abukahil <ali.abukahil@gmail.com>
2021-08-25 20:24:54 +0200

    Modelling the Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,16 +6,55 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'a tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
+  // these startDate are basically different dates at which a tour starts. For example, we can have a tour starting in December this year, and in February, the next year, and then another one in the summer and so different dates for the same tour
 });
+
 // creating a model out the Schema above
 const Tour = mongoose.model('Tour', tourSchema);
 
