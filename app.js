925e7d9a16ffe32dd3cb7691f84685109210e2a3
ali.abukahil <ali.abukahil@gmail.com>
2021-09-06 22:13:28 +0200

    Implementing a Global Error Handling Middleware

--- app.js
+++ app.js
@@ -21,10 +21,13 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // Middleware Function next
-app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
+// Just use this Middleware here in order to demonstrate
+// the concept of Middleware
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware 👋');
+//   next();
+// });
+
 // Another Middleware Function next
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString().slice(0, 16).replace('T', ' ');
@@ -37,5 +40,32 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 // users
 app.use('/api/v1/users', userRouter);
+
+// this route should be the last part after all our other routes
+// example 127.0.0.1:3000/api/tours/monkey :`)
+// in Express we can use app.all for all the HTTP method
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// an error handling middleware function
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
