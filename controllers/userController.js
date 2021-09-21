9d13095b64499768752e39fd278e59c45f711277
ali.abukahil <ali.abukahil@gmail.com>
2021-09-21 16:44:54 +0200

    Updating the Current User: Data

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,17 @@ const User = require('./../models/userModel');
 // requirung the catching errors catchAsync function from utilities folder
 const catchAsync = require('./../utils/catchAsync');
 
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  // looping with Object, easy ways to loop in JavaScript, Object.keys(obj) => returns an Array containing all the key name
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 // ) Route Handlers
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -18,6 +29,35 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     },
   });
 });
+
+// Update me handler Function
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) create error if user POSTs password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use / updateMyPassword',
+        400
+      )
+    );
+  }
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,6 +19,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 // routes in REST format
 router
   .route('/')
