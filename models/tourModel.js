2b147751f5be5f07c3f52102b385503150193257
ali.abukahil <ali.abukahil@gmail.com>
2021-09-22 23:38:40 +0200

    Modelling Tour Guides: Embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 // requiring Validator
 // const validator = require('validator');
+// requiring userModel
+const User = require('./userModel');
 // Building up a schema
 const tourSchema = new mongoose.Schema(
   {
@@ -109,6 +111,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -126,6 +129,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT Mongoose MIDDLEWARE: runs before .save() and .create() ONLY on those two Not for update()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
