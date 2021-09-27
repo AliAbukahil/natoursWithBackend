1d2d799c7343be69ad557d56fc4068af7eb4907f
ali.abukahil <ali.abukahil@gmail.com>
2021-09-27 16:34:43 +0200

    Nested Routes with Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,14 +3,17 @@ const express = require('express');
 const tourController = require('../controllers/tourController');
 // reqiuring the authController
 const authController = require('../controllers/authController');
-// requiring the review controller
-const reviewController = require('../controllers/reviewController');
+// requiring the review Router
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
 // the param Middleware
 // in a param middleware function we actually get access to a fourth argument example : val
 // router.param('id', tourController.checkID);
+
+// This is called mounting a router
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap') // aliasTopTours is a middleware function which will manipulate the query object that's coming in
@@ -34,12 +37,13 @@ router
     tourController.deleteTour
   );
 
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
-  );
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
 // When we have one thing to export we use module.exports
 module.exports = router;
