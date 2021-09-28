2848659f1c488bfaf23f60457df30f5859bf2b51
ali.abukahil <ali.abukahil@gmail.com>
2021-09-28 21:17:23 +0200

    Factory Functions: Update and Create

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -32,3 +32,37 @@ exports.deleteOne = (Model) =>
 //     data: null,
 //   });
 // });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    //patch Http method
+    // querying for the document that we want to update
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+// Post Http method
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
