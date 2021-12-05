fa7cb0a358316a163f42ffc3e8b5ad0059716a1a
ali.abukahil <ali.abukahil@gmail.com>
2021-12-05 16:09:41 +0100

    Updating User Password with Our API

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,14 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
-import { updateData } from './upadateSettings';
+import { upadateSettings } from './upadateSettings';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.getElementById('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // DELEGATION
 if (mapBox) {
@@ -33,5 +34,26 @@ if (userDataForm)
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
+    upadateSettings({ name, email }, 'data');
+  });
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    // this 'Updating...' will appear to the user while changing the password on the button
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await upadateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+    // setting the button text back to 'Save password' after finishing the changing and encryption of the password
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    // resetting the fields back to empty after changing the password
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
