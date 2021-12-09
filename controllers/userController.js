c9e990e2275348610d3ad0d6172f515975d2abc1
ali.abukahil <ali.abukahil@gmail.com>
2021-12-09 11:48:59 +0100

    Resizing Images

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,20 @@
-// requiring Multer
 const multer = require('multer');
-// requiring the userModal from the modals folder
-//const { create } = require('./../models/userModel');
-const User = require('../models/userModel');
-// requirung the catching errors catchAsync function from utilities folder
-const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-// requiring the handler Factory delete function
+const sharp = require('sharp');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/img/users');
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+// const multerStorage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: (req, file, cb) => {
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// });
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -34,8 +31,21 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
+
 const filterObj = (obj, ...allowedFields) => {
-  // looping with Object, easy ways to loop in JavaScript, Object.keys(obj) => returns an Array containing all the key name
   const newObj = {};
   Object.keys(obj).forEach((el) => {
     if (allowedFields.includes(el)) newObj[el] = obj[el];
@@ -48,17 +58,17 @@ exports.getMe = (req, res, next) => {
   next();
 };
 
-// Update me handler Function
 exports.updateMe = catchAsync(async (req, res, next) => {
-  // 1) create error if user POSTs password
+  // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
-        'This route is not for password updates. Please use / updateMyPassword',
+        'This route is not for password updates. Please use /updateMyPassword.',
         400
       )
     );
   }
+
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
   if (req.file) filteredBody.photo = req.file.filename;
@@ -77,11 +87,10 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
-// to to deactivate the user
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
 
-  res.status(200).json({
+  res.status(204).json({
     status: 'success',
     data: null,
   });
@@ -97,6 +106,6 @@ exports.createUser = (req, res) => {
 exports.getUser = factory.getOne(User);
 exports.getAllUsers = factory.getAll(User);
 
-// DO NOT update passwords with this!
+// Do NOT update passwords with this!
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,17 @@
 const express = require('express');
-// requiring the user controller
-const userController = require('../controllers/userController');
-// requiring the auth controller
-const authController = require('../controllers/authController');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-// a route for signup
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
-router.post('/forgotPassword', authController.forgotPassword); // only receive the email address
-router.patch('/resetPassword/:token', authController.resetPassword); // will receive the token as well as the new password
 
-// this here is a nice little trick in order to protect all of the routes at the same time
-// typically by using a middleware that comes before all these other routes.
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Protect all routes after this middleware
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
@@ -22,24 +19,22 @@ router.get('/me', userController.getMe, userController.getUser);
 router.patch(
   '/updateMe',
   userController.uploadUserPhoto,
+  userController.resizeUserPhoto,
   userController.updateMe
 );
 router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
 
-// routes in REST format
 router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
-// routes in REST format
 router
   .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
 
-// When we have one thing to export we module.exports
 module.exports = router;
