62e54a93df8aea20f3274843a1fdc38db92f8c3f
ali.abukahil <ali.abukahil@gmail.com>
2022-01-09 23:28:30 +0100

    Integrating Stripe into the Back-End

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-// requiring mongoose package
 const mongoose = require('mongoose');
-// requiring dotenv
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
@@ -9,26 +7,22 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-//Environment Variables
 dotenv.config({ path: './config.env' });
-// requiring the app file
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
-// calling the connect method on mongoose
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connection successful'));
+  .then(() => console.log('DB connection successful!'));
 
-//  Start SERVER
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
