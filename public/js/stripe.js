eb9a98054a93fc85197870a17ea63f4aff880e90
ali.abukahil <ali.abukahil@gmail.com>
2022-01-18 12:45:14 +0100

    Processing Payments on the Front-End

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,22 +1,44 @@
-/* eslint-disable */
+// /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
+const stripe = Stripe(
+  'pk_test_51KG1zECMxSvGV20d0rsVEy8SyZF41hXRZ4XdFUr54ZIoFKJHYmnjwYQdhgskT3pPsV7AREqfdqiA5JKBCBzN93J200AVJCkwxL'
+);
 
-export const bookTour = async tourId => {
+export const bookTour = async (tourId) => {
   try {
-    // 1) Get checkout session from API
+    // 1) get session for the API
     const session = await axios(
       `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
     );
     console.log(session);
-
-    // 2) Create checkout form + chanre credit card
+    // 2) using Stripe Creaate checkout form + chance credit card
     await stripe.redirectToCheckout({
-      sessionId: session.data.session.id
+      sessionId: session.data.session.id,
     });
   } catch (err) {
-    console.log(err);
     showAlert('error', err);
   }
 };
+
+// import axios from 'axios';
+// import { showAlert } from './alerts';
+// const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
+
+// export const bookTour = async tourId => {
+//   try {
+//     // 1) Get checkout session from API
+//     const session = await axios(
+//       `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+//     );
+//     console.log(session);
+
+//     // 2) Create checkout form + chanre credit card
+//     await stripe.redirectToCheckout({
+//       sessionId: session.data.session.id
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     showAlert('error', err);
+//   }
+// };
