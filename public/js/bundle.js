ad79e8091bc46381ee4908e8a596bdf22464e331
ali.abukahil <ali.abukahil@gmail.com>
2022-01-18 14:51:08 +0100

    Processing Payments on the Front-End

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9034,26 +9034,7 @@ var bookTour = /*#__PURE__*/function () {
   return function bookTour(_x) {
     return _ref.apply(this, arguments);
   };
-}(); // import axios from 'axios';
-// import { showAlert } from './alerts';
-// const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
-// export const bookTour = async tourId => {
-//   try {
-//     // 1) Get checkout session from API
-//     const session = await axios(
-//       `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-//     );
-//     console.log(session);
-//     // 2) Create checkout form + chanre credit card
-//     await stripe.redirectToCheckout({
-//       sessionId: session.data.session.id
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     showAlert('error', err);
-//   }
-// };
-
+}();
 
 exports.bookTour = bookTour;
 },{"axios":"../../node_modules/axios/index.js","./alerts":"alerts.js"}],"index.js":[function(require,module,exports) {
